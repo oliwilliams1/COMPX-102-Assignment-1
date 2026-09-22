2982aba53e66d911f5276859d2bfde0dc1faf894
Oli Williams <oli@oliwilliams.dev>
2026-09-22 17:25:17 +1200

    gate parent class

--- Circuits/AndGate.cs
+++ Circuits/AndGate.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,33 +12,8 @@ namespace Circuits
     /// This class implements an AND gate with two inputs
     /// and one output.
     /// </summary>
-    public class AndGate
+    public class AndGate : Gate
     {
-        // left is the left-hand edge of the main part of the gate.
-        // So the input pins are further left than left.
-        protected int left;
-
-        // top is the top of the whole gate
-        protected int top;
-
-        // width and height of the main part of the gate
-        protected const int WIDTH = 40;
-        protected const int HEIGHT = 40;
-        // length of the connector legs sticking out left and right
-        protected const int GAP = 10;
-
-        protected Brush selectedBrush = Brushes.Red;
-        protected Brush normalBrush = Brushes.LightGray;
-
-        /// <summary>
-        /// This is the list of all the pins of this gate.
-        /// An AND gate always has two input pins (0 and 1)
-        /// and one output pin (number 2).
-        /// </summary>
-        protected List<Pin> pins = new List<Pin>();
-        //Has the gate been selected
-        protected bool selected = false;
-
         /// <summary>
         /// Initialises the Gate.
         /// </summary>
@@ -50,63 +26,15 @@ namespace Circuits
             pins.Add(new Pin(this, true, 20));
             //Add the output pin to the gate
             pins.Add(new Pin(this, false, 20));
-            //move the gate and the pins to the position passed in
+
             MoveTo(x, y);
-        }
-
-        /// <summary>
-        /// Gets and sets whether the fate is selected or not.
-        /// </summary>
-        public bool Selected
-        {
-            get { return selected; }
-            set { selected = value; }
-        }
-
-        /// <summary>
-        /// Gets the left hand edge of the gate.
-        /// </summary>
-        public int Left
-        {
-            get { return left; }
-        }
-
-        /// <summary>
-        /// Gets the top edge of the gate.
-        /// </summary>
-        public int Top
-        {
-            get { return top; }
-        }
-
-        /// <summary>
-        /// Gets the list of pins for the gate.
-        /// </summary>
-        public List<Pin> Pins
-        {
-            get { return pins; }
-        }
-
-        /// <summary>
-        /// Checks if the gate has been clicked on.
-        /// </summary>
-        /// <param name="x">The x position of the mouse click</param>
-        /// <param name="y">The y position of the mouse click</param>
-        /// <returns>True if the mouse click position is inside the gate</returns>
-        public bool IsMouseOn(int x, int y)
-        {
-            if (left <= x && x < left + WIDTH
-                && top <= y && y < top + HEIGHT)
-                return true;
-            else
-                return false;
         }
 
         /// <summary>
         /// Draws the gate in the normal colour or in the selected colour.
         /// </summary>
         /// <param name="paper"></param>
-        public void Draw(Graphics paper)
+        public override void Draw(Graphics paper)
         {
             Brush brush;
             //Check if the gate has been selected
@@ -140,7 +68,7 @@ namespace Circuits
         /// </summary>
         /// <param name="x">The x position to move the gate to</param>
         /// <param name="y">The y position to move the gate to</param>
-        public void MoveTo(int x, int y)
+        public override void MoveTo(int x, int y)
         {
             //Debugging message
             Console.WriteLine("pins = " + pins.Count);
